ea97e2c1e8e7df5bd042acf620844fe4a69442d4
Pooja Ramyad <pooja.ramyad@finalmile.io>
2021-02-04 09:21:12 +1300

    Form updates

--- src/components/slices/FormSlice.vue
+++ src/components/slices/FormSlice.vue
@@ -1,16 +1,21 @@
 <template>
-  <prismic-embed :field="fields.contactform"/>
+  <span v-html="slice.primary.embed_field"></span>
   
 </template>
 
-<script>
+<script type="text/javascript">
 export default {
   props: ['slice'],
-  name: 'form-slice',
+  name: 'form-slice', 
+}
+</script>
 
-  
-  
+<style scoped>
+
+.embed_field{
+  width:100px;
+  height: 100px;
 }
 
+</style>
 
-</script>
